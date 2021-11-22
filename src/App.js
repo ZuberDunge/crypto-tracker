4fdfd78c7bea78fff181d6cafe50282a0e7baeb1
ZuberDunge <iamZuberDunge@gmail.com>
2021-11-22 13:54:49 +0530

    UPdate

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import React from 'react';
-import StudentData from './Components/StudentData';
-import Home from './Components/HomePage';
-import Main from './Main';
-import Contact from './Components/Contact';
-import AddNew from './Components/AddNew';
-import StudentContextProvider from './Components/Context/StudentContext';
-import StudentDataView from './Components/StudentDataView';
+import Main from "./Main"
 function App() {
   return (
     <div className="app">
-      <StudentContextProvider>
-        <Router>
-          <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route path='/students' element={<Main />} />
-            <Route path='/students/view/:id' element={<StudentDataView />} />
-            <Route path='/students/edit/:id' element={<StudentData />} />
-            <Route path='/contact' element={<Contact />} />
-            <Route path='/add-new' element={<AddNew />} />
-          </Routes>
-        </Router >
-      </StudentContextProvider>
+      <Main />
     </div>
   )
 }
--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,29 @@
-import ListOfStudents from './Components/ListStudents';
-import Nav from './Components/Navbar';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import StudentData from './Components/StudentData';
+import Home from './Components/HomePage';
+import Main from './Main';
+import Contact from './Components/Contact';
+import AddNew from './Components/AddNew';
+import StudentContextProvider from './Components/Context/StudentContext';
+import StudentDataView from './Components/StudentDataView';
 function Main() {
-    return (<div>
-        <Nav />
-        <ListOfStudents />
-    </div>
+    return (
 
-    );
+        <StudentContextProvider>
+            <Router>
+                <Routes>
+                    <Route exact path='/' element={<Home />} />
+                    <Route path='/students' element={<Main />} />
+                    <Route path='/students/view/:id' element={<StudentDataView />} />
+                    <Route path='/students/edit/:id' element={<StudentData />} />
+                    <Route path='/contact' element={<Contact />} />
+                    <Route path='/add-new' element={<AddNew />} />
+                </Routes>
+            </Router >
+        </StudentContextProvider>
+
+    )
 }
 
-export default Main;
+export default Main
