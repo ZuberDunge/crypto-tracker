dadba3898c87ae7e5c6c3b351a1dea52ef8f55d4
ZuberDunge <iamZuberDunge@gmail.com>
2021-11-22 14:00:03 +0530

    fix : router error

--- src/Main.js
+++ src/Main.js
@@ -1,29 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import React from 'react';
-import StudentData from './Components/StudentData';
-import Home from './Components/HomePage';
-import Main from './Main';
-import Contact from './Components/Contact';
-import AddNew from './Components/AddNew';
-import StudentContextProvider from './Components/Context/StudentContext';
-import StudentDataView from './Components/StudentDataView';
+import ListOfStudents from './Components/ListStudents';
+import Nav from './Components/Navbar';
+
 function Main() {
-    return (
+    return (<div>
+        <Nav />
+        <ListOfStudents />
+    </div>
 
-        <StudentContextProvider>
-            <Router>
-                <Routes>
-                    <Route exact path='/' element={<Home />} />
-                    <Route path='/students' element={<Main />} />
-                    <Route path='/students/view/:id' element={<StudentDataView />} />
-                    <Route path='/students/edit/:id' element={<StudentData />} />
-                    <Route path='/contact' element={<Contact />} />
-                    <Route path='/add-new' element={<AddNew />} />
-                </Routes>
-            </Router >
-        </StudentContextProvider>
-
-    )
+    );
 }
 
-export default Main
+export default Main;
