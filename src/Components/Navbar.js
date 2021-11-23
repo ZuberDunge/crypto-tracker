d697f4ecf6864c80174ed4dcfb627fbd461ab514
ZuberDunge <iamZuberDunge@gmail.com>
2021-11-23 17:15:51 +0530

    fix : minor change

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ import "../App.css"
 function Nav() {
     return (
         <nav className="navbar">
-            <h2>CryptoKnight APP</h2>
+            <h2>CryptoKnight</h2>
             {/* <h4>
                 <a target="_blank" rel="noreferrer" href="https://zuberdunge.tech/">Zuber Dunge <i class="fas fa-external-link-alt"></i></a></h4> */}
         </nav>
