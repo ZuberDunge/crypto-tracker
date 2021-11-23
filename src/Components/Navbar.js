e03e607af4c65b395ff694cc90681961aae8f46c
ZuberDunge <iamZuberDunge@gmail.com>
2021-11-23 18:56:53 +0530

    update : back btn

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import { useNavigate } from "react-router-dom"
 import "../App.css"
 
 function Nav() {
+    let back = useNavigate();
     return (
         <nav className="navbar">
-            <h2>CryptoKnight</h2>
+
+            <div onClick={() => back("/")} className="back-icon"><i class="fas fa-arrow-circle-left"></i></div>
+            <h2> CryptoKnight</h2>
             {/* <h4>
                 <a target="_blank" rel="noreferrer" href="https://zuberdunge.tech/">Zuber Dunge <i class="fas fa-external-link-alt"></i></a></h4> */}
         </nav>
